e916395d972ad3b4152011958c75713a61bdef68
pnganga05 <pnganga05@gmail.com>
2017-03-06 19:20:23 +0300

    solution for issue 004 simple grading system

--- issue-004/solution.js
+++ issue-004/solution.js
@@ -1,3 +1,15 @@
-function grade(a) {
-    // implement your solution here
+function grade(s) {
+    if (s <= 100 && s >= 80) {
+        return 'A';
+    } else if (s < 80 && s >= 60) {
+        return 'B';
+    } else if (s < 60 && s >= 50) {
+        return 'C';
+    } else if (s < 50 && s >= 40) {
+        return 'D';
+    } else if (s < 40 && s >= 30) {
+        return 'E';
+    } else if (s >= 0 && s < 30) {
+        return 'F';
+    }
 }
