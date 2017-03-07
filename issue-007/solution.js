6f23380faad9c113537bc2b8d6a58d3568bcdf2e
pnganga05 <pnganga05@gmail.com>
2017-03-07 11:01:57 +0300

    solution for issue 007 super sum

--- issue-007/solution.js
+++ issue-007/solution.js
@@ -1,4 +1,24 @@
-// implement your solution here
-function superSum() {
-
+function superSum(a) {
+    // create an empty array, eachNumberArray that will hold each number that will be extracted from each array value
+    var eachNumberArray = [];
+    // Loop through the array and extract each value, then turn it into a string
+    var keyTotal = a.forEach(function(key) {
+            var keyAsAString = key.toString();
+            // Loop through each extracted value(now a string) and push it to the array we created above
+            for (var i = 0; i < keyAsAString.length; i++) {
+                var eachNumber = keyAsAString.charAt(i);
+                console.log(eachNumber);
+                eachNumberArray.push(Number(eachNumber));
+            }
+        })
+        // create a function arraySum that takes an array as an argument, adds all it's values and returns the sum
+    var arraySum = function(myArray) {
+        var sum = myArray.reduce(add, 0);
+        function add(a, b) {
+            return a + b;
+        }
+        return sum;
+    };
+    // return the sum
+    return arraySum(eachNumberArray);
 }
