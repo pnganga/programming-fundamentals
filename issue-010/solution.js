5c467fd4601ca1e827b83be4ceeec2fafc07e8b6
pnganga05 <pnganga05@gmail.com>
2017-03-08 12:40:11 +0300

    solution for drill 1.9a array reduce

--- issue-010/solution.js
+++ issue-010/solution.js
@@ -1,4 +1,28 @@
 // implement your solution here
 function evenOdd(a) {
-
+    function sumOfEven(a) {
+        var sum = a.reduce(add, 0);
+        function add(a, b) {
+            if (b % 2 === 0) {
+                return a + b;
+            } else if (b % 2 !== 0) {
+                b = 0;
+                return a + b;
+            }
+        }
+        return sum;
+    }
+    function sumOfOdd(a) {
+        var sum = a.reduce(add, 0);
+        function add(a, b) {
+            if (b % 2 !== 0 || b === 0) {
+                return a + b;
+            } else if (b % 2 === 0) {
+                b = 0;
+                return a + b;
+            }
+        }
+        return sum;
+    }
+    return sumOfEven(a) - sumOfOdd(a);
 }
