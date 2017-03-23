9146e018d5cce204fd423547fbe7a368540f8da8
pnganga05 <pnganga05@gmail.com>
2017-03-23 10:56:14 +0300

    solution for issue 023, Person constructor

--- issue-023/solution.js
+++ issue-023/solution.js
@@ -1,4 +1,12 @@
 // implement your solution here
 function Person(name, age) {
-
+    this.name = name;
+    this.age = age;
+    this.walked = 0;
+}
+Person.prototype.greet = function(p2) {
+    return "hello " + p2.name;
+}
+Person.prototype.walk = function(d) {
+    return this.walked += d;
 }
