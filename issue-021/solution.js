c2fa47356d99ea18d19e4ff8f0f35ab144888a55
pnganga05 <pnganga05@gmail.com>
2017-03-24 12:06:38 +0300

    solution for issue 021, old and young

--- issue-021/solution.js
+++ issue-021/solution.js
@@ -1,4 +1,15 @@
 // implement your solution here
 function oldAndYoung(students) {
-
+	var obj = {};
+	var oldest = students[0];
+	var youngest = students[0];
+	students.forEach(function (student) {
+		if (student.yearOfBirth < oldest.yearOfBirth) {
+			oldest = student;
+		}else if (student.yearOfBirth > youngest.yearOfBirth) {
+			youngest = student;
+		}
+	})	
+	obj = {"youngest": youngest.name, "oldest": oldest.name};
+	return obj;
 }
