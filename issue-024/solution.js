1af51b2750d18dcb6a4fb6158c62d59ec05ae625
pnganga05 <pnganga05@gmail.com>
2017-04-01 09:59:15 +0300

    solution for issue 024, most frequent word

--- issue-024/solution.js
+++ issue-024/solution.js
@@ -1,4 +1,31 @@
 // implement your solution here
 function mostFrequent(s) {
-
+    var wordList = s.toLowerCase().split(/[ (),!.";:-]+/).filter(Boolean).sort();
+    var result = {},
+        frequentWord = wordList[0],
+        count = 1,
+        frequentWordList = [];
+    wordList.forEach(function(item) {
+        if (item > " ") {
+            if (!result[item]) {
+                result[item] = 1;
+            } else {
+                result[item]++;
+            }
+            if (result[item] >= count) {
+                frequentWord = item;
+                count = result[item];	
+                frequentWordList.push(frequentWord);
+            }
+        }
+    })
+    frequentWordList.sort();
+    frequentWordList.forEach(function(item) {
+        if (result[item] > result[frequentWordList[0]]) {
+            frequentWord = item;
+        } else {
+            frequentWord = frequentWordList[0];
+        }
+    })
+    return frequentWord;
 }
